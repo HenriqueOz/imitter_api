c94c1c978cc8475862f3c6281b2123cb9971ac05
Peguinm <pedroozorio30@gmail.com>
2024-12-09 08:35:05 -0300

    first commit

---
+++ app/database/database.sql
@@ -0,0 +1,48 @@
+
+DROP DATABASE sm_database;
+CREATE DATABASE sm_database;
+
+USE sm_database;
+
+CREATE TABLE IF NOT EXISTS user (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(15) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password TEXT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT UC_email UNIQUE(email),
+    CONSTRAINT UC_name UNIQUE(name)
+);
+
+CREATE TABLE IF NOT EXISTS post (
+	id INT NOT NULL AUTO_INCREMENT,
+    content VARCHAR(500) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT FK_post_user FOREIGN KEY(user_id)
+        REFERENCES user(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+	id INT NOT NULL AUTO_INCREMENT,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT FK_likes_post FOREIGN KEY(post_id)
+		REFERENCES post(id)
+		ON DELETE CASCADE,
+	CONSTRAINT FK_likes_user FOREIGN KEY(user_id)
+		REFERENCES user(id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS post_comment (
+	id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    comment VARCHAR(500),
+    PRIMARY KEY(id),
+    CONSTRAINT FK_post_comment_user FOREIGN KEY(user_id)
+		REFERENCES user(id)
+        ON DELETE CASCADE
+);
---
+++ app/go.mod
@@ -0,0 +1,12 @@
+module example/hello
+
+go 1.23.4
+
+require rsc.io/quote v1.5.2
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
